1adb26f1cb1e057edf9df3c9ceff84f3af672abc
MattCao <mattycao@gmail.com>
2014-03-02 18:51:03 -0800

    Finish the same tree

---
+++ Tree/Symmetric Tree/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        return sym(root.left, root.right);
+    }
+    private boolean sym(TreeNode left, TreeNode right) {
+        if(left == null && right == null)
+            return true;
+        if(left == null || right == null)
+            return false;
+        return left.val == right.val && sym(left.left, right.right) && sym(left.right, right.left);
+    }
+}
